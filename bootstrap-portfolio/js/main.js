6e68ebd02548d8d12cb3318ec86f0e701cee7dbb
Damianopipitone <damianopipitone33@gmail.com>
2023-04-07 10:29:32 +0200

    Freelancer

---
+++ bootstrap-portfolio/js/main.js
@@ -0,0 +1,12 @@
+// 1. Stabiliamo il prezzo del lavoro all'ora
+// 1.1 collegare il prezzo al tipo di lavoro
+// 2. Inserire i codici sconto del 25% e fare in modo di usarne solo una volta per codice
+// 3. Quando si preme send viene fuori il prezzo finale del preventivo
+
+const backEndPrice = (20,50);
+
+const frontEndPrice = (15,30);
+
+const projectAnalysisPrice = (33,60);
+
+
